e072c62bc83feda2725eb29ca6097b079a4e7967
Preston Wallace <wallace.preston@gmail.com>
2023-02-25 06:27:36 -0800

    add google analytics ga4

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,34 @@ module.exports = {
     },
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-QVB7R5W66F", // Google Analytics / GA Tag (sends data to Google Analytics)
+          "4656063220", // Google Analytics / Measurement ID or Stream ID (identifies this site in Google Analytics)
+          "UA-134507377-1", // Google Analytics / Old Universal Analytics (can get rid of this eventually)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+          // Delays processing pageview events on route update (in milliseconds)
+          delayOnRouteUpdate: 0,
+        },
+      },
+    },
     'gatsby-plugin-postcss',
     `gatsby-plugin-image`,
     {
