8ba03dc1609f383bc57c2e7023e6b71ac8569ee8
Preston Wallace <wallace.preston@gmail.com>
2023-03-31 10:14:36 -0700

    dev rocket logo additions

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Code with Confidence | Preston Wallace | Dev Rocket`,
+    title: `Code Your Future | Preston Wallace | Dev Rocket`,
     author: {
       name: `Preston Wallace`,
       summary: `a Full Stack Software Engineer with a longstanding love for everything tech. I currently work as a Software Engineer at MNTN, building solutions in Javascript, Node, GraphQL, and SQL.`,
